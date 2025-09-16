571abf9f4b7c0601de72b34922ae679c1dbfa169
andrewb-codes <232477619+andrewb-codes@users.noreply.github.com>
2025-09-16 04:40:59 +0300

    chore: init project skeleton

---
+++ src/ru/andrewb/charm/back/controller/ProfileControllet.java
@@ -0,0 +1,17 @@
+package ru.andrewb.charm.back.controller;
+
+import ru.andrewb.charm.back.service.ProfileService;
+
+public class ProfileControllet {
+
+    private final ProfileService service;
+
+    public ProfileControllet(ProfileService service) {
+        this.service = service;
+    }
+
+//    public String work(String request) {
+//
+//    }
+
+}
---
+++ src/ru/andrewb/charm/back/dao/ProfileDao.java
@@ -0,0 +1,31 @@
+package ru.andrewb.charm.back.dao;
+
+import ru.andrewb.charm.back.model.Profile;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ProfileDao {
+
+    private final ConcurrentHashMap<Long, Profile> storage;
+    private final AtomicLong idStorage;
+
+    public ProfileDao() {
+        this.storage = new ConcurrentHashMap<>();
+        this.idStorage = new AtomicLong();
+    }
+
+    public Profile save(Profile profile) {
+        long id = idStorage.incrementAndGet();
+        profile.setId(id);
+        storage.put(id, profile);
+        return profile;
+    }
+
+    public Optional<Profile> findById(Long id) {
+        return Optional.ofNullable(storage.get(id));
+    }
+
+    // TODO delete -> boolean, update -> void, findAll -> List
+}
