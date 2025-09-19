c7ccf87503e26456c37f1f4ff9c5fd9b667582cc
andrewb <232477619+andrewb-codes@users.noreply.github.com>
2025-09-20 00:26:03 +0300

    feat(http): add minimal HTTP server (thread pool) and simple HttpClient; wire server runner (#6)

--- src/ru/andrewb/charm/back/CharmBackServerRunner.java
+++ src/ru/andrewb/charm/back/CharmBackServerRunner.java
@@ -4,13 +4,7 @@ import ru.andrewb.charm.back.controller.ProfileController;
 import ru.andrewb.charm.back.dao.ProfileDao;
 import ru.andrewb.charm.back.service.ProfileService;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import static ru.andrewb.charm.back.model.Commands.*;
 
 public class CharmBackServerRunner {
 
@@ -18,31 +12,7 @@ public class CharmBackServerRunner {
 
         ProfileController controller = new ProfileController(new ProfileService(new ProfileDao()));
 
-        try (ServerSocket serverSocket = new ServerSocket(8080);
-             Socket socket = serverSocket.accept();
-             DataInputStream requestStream = new DataInputStream(socket.getInputStream());
-             DataOutputStream responseStream = new DataOutputStream(socket.getOutputStream());
-        ) {
-            String request;
-            while (!(request = requestStream.readUTF()).equals("stop")) {
-                String response;
-                if (request.startsWith(SAVE.getPrefix())) {
-                    response = controller.save(request.substring(SAVE.getPrefix().length()));
-                } else if (request.startsWith(FIND_BY_ID.getPrefix())) {
-                    response = controller.findById(request.substring(FIND_BY_ID.getPrefix().length()));
-                } else if (request.startsWith(FIND_ALL.getPrefix())) {
-                    response = controller.findAll(); // без аргументов
-                } else if (request.startsWith(UPDATE.getPrefix())) {
-                    response = controller.update(request.substring(UPDATE.getPrefix().length()));
-                } else if (request.startsWith(DELETE.getPrefix())) {
-                    response = controller.delete(request.substring(DELETE.getPrefix().length()));
-                } else {
-                    response = "Unsupported command";
-                }
-
-                System.out.println("Client request: " + request);
-                responseStream.writeUTF(response);
-            }
-        }
+        CharmHttpServer server = new CharmHttpServer(5);
+        server.start();
     }
 }
