cc17f5804106001cff4d7c769e86d7a420bceedc
andrewb-codes <232477619+andrewb-codes@users.noreply.github.com>
2025-09-22 04:49:26 +0300

    feat(servlet): registration page and profile create/update; init genders in ProfileController

---
+++ src/ru/andrewb/charm/back/controller/RegistrationController.java
@@ -0,0 +1,20 @@
+package ru.andrewb.charm.back.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.andrewb.charm.back.model.Profile;
+
+import java.io.IOException;
+
+@WebServlet("/registration")
+public class RegistrationController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("profile", new Profile());
+        req.getRequestDispatcher("/WEB-INF/jsp/profile.jsp").forward(req, resp);
+    }
+}
