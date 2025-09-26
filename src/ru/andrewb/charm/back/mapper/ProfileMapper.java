f67f91f7c460beaf4a2dcb9be196444ef970f782
andrewb-codes <232477619+andrewb-codes@users.noreply.github.com>
2025-09-27 02:23:50 +0300

    feat(dto,mapper): add ProfileSaveDto; HTTP→SaveDto (request mapper) and SaveDto→Profile (service mapper)

---
+++ src/ru/andrewb/charm/back/mapper/ProfileMapper.java
@@ -0,0 +1,29 @@
+package ru.andrewb.charm.back.mapper;
+
+import ru.andrewb.charm.back.dto.ProfileSaveDto;
+import ru.andrewb.charm.back.model.Profile;
+
+public class ProfileMapper implements Mapper<ProfileSaveDto, Profile> {
+
+    private static final ProfileMapper INSTANCE = new ProfileMapper();
+
+    private ProfileMapper() {}
+
+    public static ProfileMapper getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Profile map(ProfileSaveDto dto) {
+        Profile profile = new Profile();
+        profile.setId(dto.getId());
+        profile.setEmail(dto.getEmail());
+        profile.setName(dto.getName());
+        profile.setSurname(dto.getSurname());
+        profile.setAbout(dto.getAbout());
+        profile.setBirthDate(dto.getBirthDate());
+        profile.setGender(dto.getGender());
+
+        return profile;
+    }
+}
