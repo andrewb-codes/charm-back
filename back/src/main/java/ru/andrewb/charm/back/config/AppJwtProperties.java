e4547d87a4bc9f42d1687755ac25515764cbbd56
andrewb-codes <232477619+andrewb-codes@users.noreply.github.com>
2026-04-18 17:30:48 +0300

    fix imports

--- back/src/main/java/ru/andrewb/charm/back/config/AppJwtProperties.java
+++ back/src/main/java/ru/andrewb/charm/back/config/AppJwtProperties.java
@@ -1,9 +1,7 @@
 package ru.andrewb.charm.back.config;
 
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.FieldDefaults;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
